df9c9a282c633ee3f0e384e377d7aa83310f5f6c
Ana Hernandez <her17138@uvg.edu.gt>
2019-04-19 19:47:38 -0600

    el dalmata se tarda mas de 11 min en renderizar :(

--- principal.py
+++ principal.py
@@ -6,16 +6,25 @@
 
 import module as s
 import math 
-s.bm = s.Bitmap(2400,2400)
+#s.bm = s.Bitmap(2400,2400)
+s.bm = s.Bitmap(1920, 1080)
 light = s.Vector3(0,1,1)
+background = s.Texture("farm.bmp")
+s.bm.loadBackground(background)
+
+tdog = s.Texture("dalmata.bmp")
+s.bm.load("dalmata.obj", "dalmata.mtl", tdog, (-0.5,0,0), (1, 2, 1), (0,0,0), (0,1,5), (0,1,0),(0,0,0), light)
+
 '''
 tcat = s.Texture("cattexture.bmp")
 
 #             obj      mtl   texture  translate         scale            rotate         eye       up   center      light
-s.bm.load("cat.obj", "cat.mtl", tcat, (-0.5,0,0), (0.45,0.45,0.45), (0,math.pi/4,0), (0,1,5), (0,1,0),(0,0,0), s.Vector3(-1.5,1,5))
+s.bm.load("cat.obj", "cat.mtl", tcat, (0.1,0.2,-0.3), (1, 2, 1), (0,-math.pi/4,0), (0,1,5), (0,1,0),(0,0,0), s.Vector3(-1.5,1,5))
+
 '''
-tduck = s.Texture("duck.bmp")
-s.bm.load("duck.obj", "duck.mtl", tduck, (0.5,0,0), (0.45,0.45,0.45), (0,-math.pi/6,0), (0,1,5), (0,1,0),(0,0,0), light)
+
+#tduck = s.Texture("duck.bmp")
+#s.bm.load("duck.obj", "duck.mtl", tduck, (0.5,0,0), (0.45,0.45,0.45), (0,-math.pi/6,0), (0,1,5), (0,1,0),(0,0,0), light)
 '''
 tdog = s.Texture("dalmata.bmp")
 s.bm.load("dalmata.obj", "dalmata.mtl", tdog, (-0.5,0,0), (0.3,0.3,0.3), (0,0,0), (0,1,5), (0,1,0),(0,0,0), light)
